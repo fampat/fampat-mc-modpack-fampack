da5cfef1ef94e09dccbf4c8669bf228cb44ba8bb
Patrick Patzelt <mail@fampat.de>
2021-12-22 18:36:58 +0100

    MOD Added minecraft screen name

--- cfg/scripts/startup.bash
+++ cfg/scripts/startup.bash
@@ -3,5 +3,5 @@
 RUNNING_SCREEN=$(screen -ls | grep 'minecraft')
 
 if [[ ! -z RUNNING_SCREEN ]]; then
-    screen -dm bash -c "cd /home/minecraft/Fampack && ./run.sh"
+    screen -S "minecraft" -dm bash -c "cd /home/minecraft/Fampack && ./run.sh"
 fi
