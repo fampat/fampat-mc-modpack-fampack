74e4643c0873fd925968a5a234ff44e99ed00ffb
fampat <mail@fampat.de>
2021-12-23 18:24:27 +0100

    FIX Shutdown grep for screen

--- cfg/scripts/shutdown.bash
+++ cfg/scripts/shutdown.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-RUNNING_SCREEN=$(screen -ls | grep 'minecraft')
+RUNNING_SCREEN=$(screen -ls | grep -e '[/d]*\.minecraft')
 
 if [[ ! -z RUNNING_SCREEN ]]; then
     echo '>>> Stopping Minecraft server and waiting 5 minutes for completion...'
-    screen -S $(screen -ls | grep 'minecraft' | awk '{ print $1 }') -p 0 -X stuff "stop^M"
+    screen -S $(screen -ls | grep -e '[/d]*\.minecraft' | awk '{ print $1 }') -p 0 -X stuff "stop^M"
     sleep 5m
     echo '>>> Ending Minecraft screen session...'
-    screen -S $(screen -ls | grep 'minecraft' | awk '{ print $1 }') -p 0 -X stuff "exit^M"
+    screen -S $(screen -ls | grep -e '[/d]*\.minecraft' | awk '{ print $1 }') -p 0 -X stuff "exit^M"
     
     # If a param is present, also shutdown the system
     # This requires that the minecraft user has executed this script with sudo
