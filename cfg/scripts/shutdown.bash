e1434df2d1990d420914e2906cffc05e99b837e8
fampat <mail@fampat.de>
2021-12-23 17:06:49 +0100

    MOD Updated serber handling

--- cfg/scripts/shutdown.bash
+++ cfg/scripts/shutdown.bash
@@ -3,7 +3,17 @@
 RUNNING_SCREEN=$(screen -ls | grep 'minecraft')
 
 if [[ ! -z RUNNING_SCREEN ]]; then
+    echo '>>> Stopping Minecraft server and waiting 5 minutes for completion...'
     screen -S $(screen -ls | grep 'minecraft' | awk '{ print $1 }') -p 0 -X stuff "stop^M"
-    sleep 8m
+    sleep 5m
+    echo '>>> Ending Minecraft screen session...'
     screen -S $(screen -ls | grep 'minecraft' | awk '{ print $1 }') -p 0 -X stuff "exit^M"
+    
+    # If a param is present, also shutdown the system
+    # This requires that the minecraft user has executed this script with sudo
+    if [[ "$1" = "turnoff" ]]; then
+     echo '>>> Shutting down the Minecraft home-server...'
+     sleep 10s
+     shutdown now -h
+    fi
 fi
--- cfg/scripts/startup.bash
+++ cfg/scripts/startup.bash
@@ -3,5 +3,6 @@
 RUNNING_SCREEN=$(screen -ls | grep 'minecraft')
 
 if [[ ! -z RUNNING_SCREEN ]]; then
+    echo '>>> Starting the Minecraft home-server...'
     screen -S "minecraft" -dm bash -c "cd /home/minecraft/Fampack && ./run.sh"
 fi
